7316ad3c02abe228ffc471363df3e87e968a2cab
severejetlag <nhl1013@gmail.com>
2017-11-21 12:28:14 -0800

    Starting to implement FFaker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,78 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'ffaker'
+
+User.destroy_all
+UserSoftware.destroy_all
+Software.destroy_all
+
+softwaresList = [
+	{
+		name: 'Contexts',
+		tag: 'Productivity'
+	},
+	{
+		name: 'Sublime Text',
+		tag: 'Programming'
+	},
+	{
+		name: 'Adobe Photoshop',
+		tag: 'Design'
+	},
+	{
+		name: 'SizeUp',
+		tag: 'Utility'
+	},
+	{
+		name: 'Stay',
+		tag: 'Utility'
+	},
+	{
+		name: 'Slack',
+		tag: 'Communication'
+	},
+	{
+		name: 'Telegram',
+		tag: 'Communication'
+	},
+	{
+		name: 'Adobe InDesign',
+		tag: 'Design'
+	},
+	{
+		name: 'Microsoft Word',
+		tag: 'Productivity'
+	},
+	{
+		name: 'Microsoft PowerPoint',
+		tag: 'Productivity'
+	},
+	{
+		name: 'Microsoft Excel',
+		tag: 'Productivity'
+	},
+	{
+		name: 'Google Docs',
+		tag: 'Productivity'
+	},
+	{
+		name: 'Google Sheets',
+		tag: 'Productivity'
+	},
+	{
+		name: 'Google Slides',
+		tag: 'Productivity'
+	},
+	{
+		name: 'Atom',
+		tag: 'Programming'
+	},
+	{
+		name: 'Robo 3T',
+		tag: 'Programming'
+	}
+]
+
+p FFaker::Name.first_name
