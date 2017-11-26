e3c69e18c107ba8bbe818cb267dc4f15feead5ce
severejetlag <nhl1013@gmail.com>
2017-11-26 15:00:12 -0800

    Updated Seed file and made inedex route

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,70 +13,32 @@ UserSoftware.destroy_all
 Software.destroy_all
 
 softwaresList = [
-	{
-		name: 'Contexts',
-		tag: 'Productivity'
-	},
-	{
-		name: 'Sublime Text',
-		tag: 'Programming'
-	},
-	{
-		name: 'Adobe Photoshop',
-		tag: 'Design'
-	},
-	{
-		name: 'SizeUp',
-		tag: 'Utility'
-	},
-	{
-		name: 'Stay',
-		tag: 'Utility'
-	},
-	{
-		name: 'Slack',
-		tag: 'Communication'
-	},
-	{
-		name: 'Telegram',
-		tag: 'Communication'
-	},
-	{
-		name: 'Adobe InDesign',
-		tag: 'Design'
-	},
-	{
-		name: 'Microsoft Word',
-		tag: 'Productivity'
-	},
-	{
-		name: 'Microsoft PowerPoint',
-		tag: 'Productivity'
-	},
-	{
-		name: 'Microsoft Excel',
-		tag: 'Productivity'
-	},
-	{
-		name: 'Google Docs',
-		tag: 'Productivity'
-	},
-	{
-		name: 'Google Sheets',
-		tag: 'Productivity'
-	},
-	{
-		name: 'Google Slides',
-		tag: 'Productivity'
-	},
-	{
-		name: 'Atom',
-		tag: 'Programming'
-	},
-	{
-		name: 'Robo 3T',
-		tag: 'Programming'
-	}
+	{name: 'Contexts',tag: 'Productivity'},
+	{name: 'Sublime Text',tag: 'Programming'},
+	{name: 'Adobe Photoshop',tag: 'Design'},
+	{name: 'SizeUp',tag: 'Utility'},
+	{name: 'Stay',tag: 'Utility'},
+	{name: 'Slack',tag: 'Communication'},
+	{name: 'Telegram',tag: 'Communication'},
+	{name: 'Adobe InDesign',tag: 'Design'},
+	{name: 'Microsoft Word',tag: 'Productivity'},
+	{name: 'Microsoft PowerPoint',tag: 'Productivity'},
+	{name: 'Microsoft Excel',tag: 'Productivity'},
+	{name: 'Google Docs',tag: 'Productivity'},
+	{name: 'Google Sheets',tag: 'Productivity'},
+	{name: 'Google Slides',tag: 'Productivity'},
+	{name: 'Atom',tag: 'Programming'},
+	{name: 'Robo 3T',tag: 'Programming'}
 ]
 
-p FFaker::Name.first_name
+Software.create(softwaresList)
+
+
+50.times do |f|
+	first_n = FFaker::Name.first_name
+	last_n = FFaker::Name.last_name
+	email = FFaker::Internet.free_email
+	username = FFaker::Internet.user_name
+	job = FFaker::Job.title
+	User.create(first_name: first_n, last_name: last_n, email: email, username: username, job_title:job, job_field: "Tech")
+end
