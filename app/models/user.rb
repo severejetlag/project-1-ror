a438a33074adf40fa78606f38725b8d37066f746
severejetlag <nhl1013@gmail.com>
2017-11-26 19:32:13 -0800

    updated most of CRUD for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   has_many :user_softwares, dependent: :destroy
   has_many :softwares, through: :user_softwares
+
+  def self.confirm(params)
+    @user = User.find_by({email: params[:email]})
+    @user ? @user.authenticate(params[:password]) : false
+  end
 end
